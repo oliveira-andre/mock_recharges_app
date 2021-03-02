9ca4163cb7fb851bc86e27cdfc2b35006458b6ee
Andre Oliveira <desmonplays@gmail.com>
2021-03-01 23:38:25 -0300

    feat: validating if customer or recharge is denied

--- app/models/recharge.rb
+++ app/models/recharge.rb
@@ -4,4 +4,29 @@ class Recharge < ApplicationRecord
 
   belongs_to :customer
   belongs_to :card
+
+  validate :card_denied
+  validate :customer_denied
+
+  before_create :set_token
+
+  private
+
+  def card_denied
+    return unless card.denied?
+
+    @errors.add(:card_id, 'your card is blacklisted')
+  end
+
+  def customer_denied
+    return unless customer.denied?
+
+    @errors.add(:customer_id, 'you are blacklisted')
+  end
+
+  def set_token
+    self.token = JWT.encode(
+      { amount: amount, created_at: created_at }, nil, 'none'
+    )
+  end
 end
